93a4315d5a94b063fc3edd9b6a8cf8190e2f1433
Andreas <arizkirizaldi@email.unikom.ac.id>
2019-08-14 14:50:41 +0700

    Login

---
+++ app/src/main/java/xyz/kyrozyn/uasakb2019/model/LoginPreference.java
@@ -0,0 +1,12 @@
+package xyz.kyrozyn.uasakb2019.model;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginPreference {
+    AppCompatActivity a;
+
+    public LoginPreference(AppCompatActivity a) {
+        this.a = a;
+    }
+
+}
